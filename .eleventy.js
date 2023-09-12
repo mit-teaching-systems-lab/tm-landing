babd1a652fb6f988f8bda1c84507c65edf30f9e5
Rick Waldron <waldron.rick@gmail.com>
2023-09-12 09:38:08 -0400

    fix: mit-teaching-systems-lab/dcss/issues/37

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,15 @@
+const isProduction = process.env.DEPLOYMENT_ENV === 'production';
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addWatchTarget("./src/js/");
   eleventyConfig.addPassthroughCopy("./src/static");
   eleventyConfig.addFilter("keys", obj => Object.keys(obj));
   eleventyConfig.addFilter("except", (arr=[]) => arr.filter(value => value != "all").sort());
-  eleventyConfig.setBrowserSyncConfig({
+  eleventyConfig.setBrowserSyncConfig(
+    isProduction ? {
+      ghostMode: false,
+    } : {
     middleware(_req, res, next) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       next();
